241143b221a2f4dde5f31de970bf2872455213b2
物流研发部-李荣华 <lironghua@xsyxsc.com>
2021-03-01 08:36:07 +0800

    aa

---
+++ el-user-behavior-storage-provider/src/main/java/com/hyxt/Test.java
@@ -0,0 +1,12 @@
+/*
+ * xsyx Inc. 湖南兴盛优选电子商务有限公司.
+ * Copyright (c) 2017-2021. All Rights Reserved.
+ */
+package com.hyxt;
+
+/**
+ * @author lirh
+ * @date 2021/03/01 8:35
+ */
+public class Test {
+}
